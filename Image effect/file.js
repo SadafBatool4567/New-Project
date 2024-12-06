6c7d55ade67f99c7e21bfdc84fa843f086be5333
SadafBatool4567 <safafbatool@gmail.com>
2024-12-06 15:36:56 +0500

    Add files via upload

---
+++ Image effect/file.js
@@ -0,0 +1,12 @@
+function ch(){
+    document.getElementById("img").src="https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg"
+}
+function ch1(){
+    document.getElementById("img").src="https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?cs=srgb&dl=pexels-pixabay-257360.jpg&fm=jpg"
+}
+function ch2(){
+    document.getElementById("img").src="https://img.pikbest.com/photo/20240608/spring-season-concept-3d-bird-in-nature-with-tree-flower_10603856.jpg!f305cw"
+}
+function ch3(){
+    document.getElementById("img").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtC3uNAsV6WQ9NyyfECP0aPtOJ-zWV-lNIMklQbKlI6p_Sry7_21_LPZaaDZ78FbSf5sY&usqp=CAU"
+}
